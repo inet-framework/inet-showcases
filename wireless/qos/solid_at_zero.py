d5ae8ab1922a5fc48699515797cf41a8cc3a9723
Gyorgy Szaszko <adam.szaszko@gmail.com>
2019-03-25 15:56:43 +0100

    showcases: added IEEE 802.11 Quality of Service showcase

---
+++ wireless/qos/solid_at_zero.py
@@ -0,0 +1,21 @@
+def solid_at_zero(ax, default_color):
+    a = ax.get_ygridlines()
+    ticklocs = ax.get_yticks()
+    ticklabels = ax.get_yticklabels()
+    #print("gridlines:",a)
+    
+    j = 0
+    r = 0
+    # set everything to default here
+    ax.grid(linestyle='--',color=default_color)
+    
+    for i in ticklocs:
+        if i == 0.0:
+            #print("found 0 at ",j)
+            r = j
+            b = a[r]
+            b.set_color('black')
+            b.set_linestyle('solid')
+        j += 1
+    
+#solid_at_zero()
